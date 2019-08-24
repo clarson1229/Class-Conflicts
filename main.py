c08be9a14857ca7913fd648168d6b11f86126112
Clarson1229 <27305718+clarson1229@users.noreply.github.com>
2019-08-24 14:57:55 -0500

    added menu and wrote conflict part

--- main.py
+++ main.py
@@ -17,16 +17,21 @@ class Days(Enum):
     TU = 4
     F = 5
 
+class Student(object):
+    def __init__(self, name: str='', classList: list=[]):
+        self.name = name
+        self.classList = classList
+
 
 class ClassStruct(object):
-    def __init__(self, name: str='', days: list = [], startT: str='', endT: str=''):
+    def __init__(self, startT , endT, name: str='', days: list = []):
         self.name = name
         self.days = days
         self.startT = startT
         self.endT = endT
 
 
-def open_file(fileName: str) -> dict:
+def open_file(fileName: str, studentList: dict) -> dict:
     """ Opens CSV file and reads   """
     try:
         # opens file and uses CSV reader to get each line into a list then iterates through and stores into a dictionary
@@ -52,14 +57,120 @@ def open_file(fileName: str) -> dict:
             if tempEndTime[1] == 'PM' and eHour != 12:
                 eHour += 12
             # print('End:' + str(eHour) + ':' + str(eMin))
+            tStart = datetime.time(sHour, sMin)
+            tEnd = datetime.time(eHour,eMin)
 
+            tempClass = ClassStruct(tStart, tEnd, className, days)
+
+            if tempName in studentList.keys() :
+                studentList[tempName].append(tempClass)
+            else:
+                studentList[tempName] = [tempClass]
 
         file.close()
+        return studentList
     except IOError:
         print("File not found Enter a new file")
 
+def find_available(studentList, day, startT, endT ):
+    for student in studentList:
+        available = True
+        for className in studentList[student]:
+            if day in className.days:
+                if max(startT, className.startT) < min(endT, className.endT):
+                    available = False
+                    print(student, 'has a Conflict with class ', className.name, 'at ', className.startT, className.endT , ' with interval', startT, endT,)
+        if available:
+            print(student, 'is available')
+#                 no classes on that day and therefore available
+
+def getData(prompt, targetData, warning)-> str:
+    """Given the prompt and target Data function will run until given data is in target data range"""
+    data = ''
+    while True:
+        data = input(prompt).upper().strip()
+        # if the user just presses enter it catches it
+        if data == '':
+            print(warning)
+        # if the given input is one of the accepted values
+        elif data in targetData:
+            return data
+        else:
+            print(warning)
+
+def getTime(prompt, warning)-> str:
+    data =''
+    while True:
+        data = input(prompt).upper().strip()
+        # if the user just presses enter it catches it
+        if data == '':
+            print(warning)
+        # if the given input is one of the accepted values
+        elif ':' in data :
+            if ' ' in data:
+                if 'AM' or 'PM' in data:
+                    print('true')
+                    return data
+
+        else:
+            print(warning)
+
+def formatTime(time)-> datetime:
+    tempTime = time.split(' ')
+    hour, min = tempTime[0].split(':')
+    hour = int(hour)
+    min = int(min)
+
+    if tempTime[1] == 'PM' and hour != 12:
+        hour += 12
+    returnTime = datetime.time(hour, min)
+    return returnTime
+
 
 def main():
-    open_file('classes.csv')
+    studentList = {}
+    studentList = open_file('classes.csv', studentList)
+    # for student in studentList:
+    #     for className in studentList[student]:
+    #         print(className.name, className.days, className.startT, className.endT)
+
+    running = 1
+    while running == 1:
+        # info for the running or not function
+        runPrompt = """      Class Viewer \n
+        1. Open Check Conflicts
+        Q. Quit
+    >>"""
+        runWarning = "Input Must be 1 or Q"
+        runTarget = "1,Q"
+        program = getData(runPrompt, runTarget, runWarning)
+        if program == "1":
+            # getting input variables
+            # Day
+            dayPrompt = """      Enter the Day You want to check M-T-W-R-F
+    >>"""
+            dayWarning = "Input Must be one of the following: M-T-W-R-F"
+            dayTarget = "M,T,W,R,F"
+            dayToCheck = getData(dayPrompt, dayTarget, dayWarning)
+            # start time
+            timePrompt = """      Time Range Start? (##:## AM/PM)
+    >> """
+            timeWarning = "Input Must be in the format (12:00 PM) the time separated by :, space, AM or PM "
+            intervalStart = getTime(timePrompt, timeWarning)
+            # end time
+            timePrompt = """      Time Range End? (##:## AM/PM)
+    >> """
+            timeWarning = "Input Must be in the format (12:00 PM) the time separated by :, space, AM or PM "
+            intervalEnd = getTime(timePrompt, timeWarning)
+
+            testStart = formatTime(intervalStart)
+            testEnd   = formatTime(intervalEnd)
+            find_available(studentList, dayToCheck, testStart, testEnd)
+
+        else:
+            break
+
+
+
 
 main()
